66eaf63fc84146d347fe6a13bf5b904a529d3c8e
Bharath Raj <bharathrajpalivela@gmail.com>
2022-08-19 15:01:26 +0530

    3.1-3.6

---
+++ index.js
@@ -0,0 +1,108 @@
+const { notStrictEqual } = require('assert')
+const { response } = require('express')
+const express = require('express')
+const app = express()
+const repl = require('repl')
+
+app.use(express.json())
+
+let persons = [
+    { 
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    { 
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    { 
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.get("/", (request,response) => {response.send("main page")})
+
+app.get("/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    person ?
+    response.send(`
+    <p><b>ID</b>: ${person.id}</p>
+    <p><b>Name</b>: ${person.name}</p>
+    <p><b>Number</b>: ${person.number}</p>
+    `)
+    :
+    response.status(404).end()
+})
+
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+  ${date}`)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  person ? response.json(person) : response.status(404).end()
+})
+
+app.delete('api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
